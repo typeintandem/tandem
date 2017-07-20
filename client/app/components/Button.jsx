9ee5b2ac776fd5930b2b8bded167beed46a2deaa
Jamiboy Mohammad <jamiboy16@gmail.com>
2017-07-20 03:17:08 -0400

    Initial mocked dashboard UI (#18)

--- client/app/components/Button.jsx
+++ client/app/components/Button.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import './Button.scss';
 
 export default class Button extends React.PureComponent {
+  static get propTypes() {
+    return {
+      className: PropTypes.string,
+    };
+  }
+
+  static get defaultProps() {
+    return {
+      className: '',
+    };
+  }
+
   render() {
+    const { className, ...props } = this.props;
     return (
-      <div className="button" {...this.props} />
+      <div className={cn('button', className)} {...props} />
     );
   }
 }
