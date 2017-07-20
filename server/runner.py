8d1b340f44a0ef631460dcc78f0a176a50234daa
Geoffrey Yu <geoffxy@gmail.com>
2017-07-20 14:37:26 -0400

    Add actions to runner to handle clicking and typing (#23)
    * WIP
    
    * Custom request response handling works
    
    * Working wait for event
    
    * Navigate to new page
    
    * Working Github sign in test
    
    * Adapt to a map of attributes
    
    * Fix lint errors
    
    * Single quotes

--- server/runner.py
+++ server/runner.py
@@ -1,9 +1,16 @@
 import uuid
 import signal
-
+from subprocess import Popen
 from random import random
+from time import sleep
+
 from tandem.utilities.web_driver import WebDriver
 from tandem.utilities.job_queue_driver import WorkerDriver
+
+
+class TestFailure(BaseException):
+    def __init__(self, reason):
+        self.reason = reason
 
 
 def runner_main():
@@ -24,9 +31,115 @@ def runner_main():
         if maybe_flow_id is not None:
             flow_id = int(maybe_flow_id)
             print('Received flow id: {}'.format(flow_id), flush=True)
-            # Read from DB
-            # Run test
+            handle_flow(flow_id)
             queue_driver.mark_flow_job_completed(flow_id)
+            print('Finished processing flow id: {}'.format(flow_id),
+                  flush=True)
+
+
+def handle_flow(flow_id):
+    # 1. Read from DB
+    # 2. Start a new instance of headless chrome
+    # 4. Run the test
+    # 6. Write the result of the test
+    # 7. Shutdown chrome
+    chrome_cmd = ['google-chrome',
+                  '--headless',
+                  '--disable-gpu',
+                  '--remote-debugging-port=9222']
+
+    with Popen(chrome_cmd) as chrome:
+        try:
+            sleep(1)
+            succeeded, failure_ex = run(flow_id)
+            if succeeded:
+                print('Test passed!', flush=True)
+            else:
+                print('Test failed! - ' + failure_ex.reason, flush=True)
+            # Write to DB
+            # Notify server if needed
+        finally:
+            print('Calling chrome.terminate()', flush=True)
+            chrome.terminate()
+
+    print('Finished handling flow {}'.format(flow_id), flush=True)
+
+
+def find_node_with_most_matches(matching_nodes,
+                                matching_attributes,
+                                active_page):
+    node_ids = matching_nodes['nodeIds']
+    num_nodes = len(node_ids)
+
+    attributes = [active_page.get_attributes(node_id)['attributes']
+                  for node_id in node_ids]
+    attribute_match_count = [0 for node_id in node_ids]
+    max_index = 0
+
+    for node_index in range(num_nodes):
+        attrs = attributes[node_index]
+        for i in range(len(attrs) // 2):
+            idx = i * 2
+            attribute_name = attrs[idx]
+            attribute_value = attrs[idx + 1]
+            if (attribute_name in matching_attributes and
+                    attribute_value == matching_attributes[attribute_name]):
+                attribute_match_count[node_index] += 1
+        if (attribute_match_count[node_index] >
+                attribute_match_count[max_index]):
+            max_index = node_index
+
+    return (node_ids[max_index] if attribute_match_count[max_index] > 0
+            else None)
+
+
+def click(query, matching_attributes, wait_navigate, active_page, focus=False):
+    matching_nodes = active_page.query_selector_all(query)
+    matches = len(matching_nodes['nodeIds'])
+
+    if matches == 0:
+        raise TestFailure('Could not find the element to click!')
+
+    sign_in_node = matching_nodes['nodeIds'][0] if matches == 1 else \
+        find_node_with_most_matches(matching_nodes,
+                                    matching_attributes,
+                                    active_page)
+
+    if sign_in_node is None:
+        raise TestFailure('Could not find the element to click!')
+
+    resp = active_page.resolve_node(sign_in_node)
+
+    if 'object' not in resp or 'objectId' not in resp['object']:
+        raise TestFailure('Could not find the element to click!')
+
+    object_id = resp['object']['objectId']
+
+    # Perform the click
+    if wait_navigate:
+        active_page.enable_page_events()
+
+    if focus:
+        resp = active_page.call_function_on(object_id,
+                                            'function() { this.focus(); }')
+    else:
+        resp = active_page.call_function_on(object_id,
+                                            'function() { this.click(); }')
+
+    if wait_navigate:
+        active_page.wait_for('Page.frameStoppedLoading', 3)
+        active_page.disable_page_events()
+
+
+def type(string, active_page):
+    for ch in string:
+        active_page.dispatch_key_event('keyDown', ch)
+
+
+def page_assertion(actual_url, expected_url):
+    if not actual_url == expected_url:
+        raise TestFailure('Expected url did not match actual url ' +
+                          actual_url + ' <> ' + expected_url)
 
 
 def run(flow):
@@ -34,11 +147,41 @@ def run(flow):
     pages = driver.pages
     page = pages[list(pages)[0]]
 
-    print(page.url)
-    page.goto("https://www.facebook.com")
-    driver.reload_pages()
-    print(page.url)
-    page.goto("https://github.com")
+    try:
+        with page.connect() as active_page:
+            # 1. Navigate to starting page
+            active_page.enable_page_events()
+            active_page.goto('https://github.com')
+            active_page.wait_for('Page.frameStoppedLoading', 3)
+            active_page.disable_page_events()
+
+            # 2. Run actions
+            # a) Click "Sign in"
+            click('a.text-bold.site-header-link',
+                  {'href': '/login'}, True, active_page)
+
+            # b) Type in username
+            type('geoffxy', active_page)
+
+            # c) Focus password field
+            click('#password', {}, False, active_page, focus=True)
+
+            # d) Type in password
+            type('abc123', active_page)
+
+            # e) Click sign in
+            click('.btn.btn-primary.btn-block',
+                  {'type': 'submit'}, True, active_page)
+
+            # 3. Assertion
+            driver.reload_pages()
+            page_assertion(page.url, 'https://github.com/session')
+
+        # Return that the test passed
+        return (True, None)
+
+    except TestFailure as ex:
+        return (False, ex)
 
 
 if __name__ == '__main__':
--- server/tandem/utilities/web_driver.py
+++ server/tandem/utilities/web_driver.py
@@ -1,14 +1,132 @@
 import json
 import requests
 import websocket
+from threading import Thread, Semaphore, Lock, Condition
 
 
-def send_websocket_request(url, payload):
-    ws = websocket.create_connection(url)
-    ws.send(payload)
-    response = ws.recv()
-    ws.close()
-    return response
+class Connection:
+    def __init__(self, websocket_url):
+        self._websocket_url = websocket_url
+        self._message_id = 0
+        self._ws = None
+        self._read_thread = None
+
+        self._pending_requests = {}
+        self._pending_requests_lock = Lock()
+
+        self._waiting_events = {}
+        self._waiting_events_lock = Lock()
+
+    def __enter__(self):
+        self._ws = websocket.create_connection(self._websocket_url)
+        self._recv_thread = Thread(target=self._handle_messages)
+        self._recv_thread.start()
+        return self
+
+    def __exit__(self, *args):
+        self._ws.close()
+        self._recv_thread.join()
+        self._ws = None
+
+    def _handle_messages(self):
+        try:
+            while True:
+                message = json.loads(self._ws.recv())
+                if 'id' in message:
+                    # Response
+                    request_id = message['id']
+                    with self._pending_requests_lock:
+                        if request_id in self._pending_requests:
+                            response_list, cond = \
+                                self._pending_requests[request_id]
+                            response_list.append(message['result'])
+                            cond.release()
+                        else:
+                            print('Received a message with id {} that has ' +
+                                  'no waiting thread'.format(request_id),
+                                  flush=True)
+                elif 'method' in message:
+                    # Event
+                    event_name = message['method']
+                    with self._waiting_events_lock:
+                        if event_name in self._waiting_events:
+                            self._waiting_events[event_name].notify_all()
+                else:
+                    pass
+        except:
+            pass
+
+    def _request(self, method, params={}):
+        request_id = self._message_id
+        self._message_id += 1
+
+        request = {
+            'id': request_id,
+            'method': method,
+            'params': params,
+        }
+
+        # Condition used to wait for response
+        cond = Semaphore(0)
+
+        # Store pending request
+        with self._pending_requests_lock:
+            self._pending_requests[request_id] = ([], cond)
+
+        # Actually send the request
+        self._ws.send(json.dumps(request))
+
+        # Wait for a response
+        cond.acquire()
+
+        response = None
+
+        # Extract the response
+        with self._pending_requests_lock:
+            response = self._pending_requests[request_id][0][0]
+            self._pending_requests.pop(request_id, None)
+
+        return response
+
+    def wait_for(self, event_name, timeout):
+        with self._waiting_events_lock:
+            if event_name not in self._waiting_events:
+                self._waiting_events[event_name] = \
+                    Condition(self._waiting_events_lock)
+            self._waiting_events[event_name].wait(timeout)
+
+    def goto(self, url):
+        return self._request('Page.navigate', {'url': url})
+
+    def enable_page_events(self):
+        return self._request('Page.enable', {})
+
+    def disable_page_events(self):
+        return self._request('Page.disable', {})
+
+    def query_selector_all(self, query):
+        document_dom = self._request('DOM.getDocument', {})
+        root_node_id = document_dom['root']['nodeId']
+        return self._request('DOM.querySelectorAll',
+                             {'nodeId': root_node_id, 'selector': query})
+
+    def get_attributes(self, node_id):
+        return self._request('DOM.getAttributes', {'nodeId': node_id})
+
+    def resolve_node(self, node_id):
+        return self._request('DOM.resolveNode', {'nodeId': node_id})
+
+    def call_function_on(self, object_id, function):
+        return self._request('Runtime.callFunctionOn',
+                             {'objectId': object_id,
+                              'functionDeclaration': function})
+
+    def dispatch_key_event(self, type, text):
+        return self._request('Input.dispatchKeyEvent',
+                             {'type': type, 'text': text})
+
+    def get_navigation_history(self):
+        return self._request('Page.getNavigationHistory', {})
 
 
 class WebPage:
@@ -16,16 +134,6 @@ class WebPage:
         self._page_id = page_id
         self._page_url = page_url
         self._websocket_url = websocket_url
-        self._message_id = 0
-
-    def _request_action(self, method, params={}):
-        self._message_id += 1
-        request = {
-            "id": self._message_id,
-            "method": method,
-            "params": params,
-        }
-        return send_websocket_request(self._websocket_url, json.dumps(request))
 
     @property
     def id(self):
@@ -38,19 +146,15 @@ class WebPage:
     def copy(self, web_page):
         self._page_url = web_page._page_url
 
-    def goto(self, url):
-        response = self._request_action("Page.navigate", {"url": url})
-        return response
-
-    # Add more actions here
-    # Then call them with page.action(params)
+    def connect(self):
+        return Connection(self._websocket_url)
 
 
 class WebDriver:
-    def __init__(self, host="localhost", port="9222"):
+    def __init__(self, host='localhost', port='9222'):
         self._host = host
         self._port = port
-        self._driver_url = "http://{0}:{1}".format(self._host, self._port)
+        self._driver_url = 'http://{0}:{1}'.format(self._host, self._port)
         self._web_pages = {}
 
         self.check_connection()
@@ -64,12 +168,12 @@ class WebDriver:
         requests.get(self._driver_url)
 
     def reload_pages(self):
-        response = requests.get("{0}/json".format(self._driver_url))
+        response = requests.get('{0}/json'.format(self._driver_url))
         pages = tuple(WebPage(
-                page["id"],
-                page["url"],
-                page["webSocketDebuggerUrl"]
-            ) for page in response.json() if page["type"] == "page")
+                page['id'],
+                page['url'],
+                page.get('webSocketDebuggerUrl', None)
+            ) for page in response.json() if page['type'] == 'page')
 
         new_web_pages = {}
         for page in pages:
