d7a8c25a10666425ec419e4de37a4d20c69e31f5
Jamiboy Mohammad <jamiboy16@gmail.com>
2017-07-20 14:27:00 -0400

    PR Fixes

--- server/tandem/models/run.py
+++ server/tandem/models/run.py
@@ -7,7 +7,7 @@ class Run(BaseModel):
     __tablename__ = 'run'
     id = schema.Column(types.Integer, primary_key=True)
     flow_id = schema.Column(types.Integer, schema.ForeignKey(Flow.__table__.c.id))
-    result = schema.Column(types.String(255))
+    result = schema.Column(types.JSON)
     submit_time = schema.Column(types.Integer)
     start_time = schema.Column(types.Integer)
     complete_time = schema.Column(types.Integer)
