93b55d28e14283890c562f9f175905415963f997
Sameer Chitley <chitley.sameer@gmail.com>
2017-07-20 00:21:10 -0400

    Added recorder setup page for when URL is unset

---
+++ client/app/components/RecorderSetup.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => (
+  <div>Enter the website URL in the bar above.</div>
+);
--- client/app/pages/recorder/RecorderApp.jsx
+++ client/app/pages/recorder/RecorderApp.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+
 import RecorderBar from 'components/RecorderBar';
+import RecorderSetup from 'components/RecorderSetup';
 import RecorderWebView from 'components/RecorderWebView';
 
 export default class RecorderApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: null,
+    };
+  }
+
+
   render() {
+    const renderSetup = () =>
+      (this.state.url ? null : <RecorderSetup />);
+
+    const renderWebView = () =>
+      (!this.state.url ? null : <RecorderWebView url={this.state.url} />);
+
     return (
       <div className="recorder">
-        <RecorderBar url={'http://www.jamiboy.com/'} clickDone={() => {}} clickSteps={() => {}} />
-        <RecorderWebView url={'http://www.jamiboy.com/'} />
+        <RecorderBar url={this.state.url} clickDone={() => {}} clickSteps={() => {}} />
+        { renderSetup() }
+        { renderWebView() }
       </div>
     );
   }
