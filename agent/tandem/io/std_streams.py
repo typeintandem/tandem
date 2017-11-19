c687803d26750c095288d29726fc80d5e8d970de
Geoffrey Yu <geoffxy@gmail.com>
2017-11-19 00:43:56 -0800

    Remove executor for StdStreams.write

--- agent/tandem/io/std_streams.py
+++ agent/tandem/io/std_streams.py
@@ -4,36 +4,20 @@ from concurrent.futures import ThreadPoolExecutor
 
 class StdStreams:
     def __init__(self, handler_function):
-        self._shutting_down = False
         self._handler_function = handler_function
         self._reader = ThreadPoolExecutor(max_workers=1)
-        self._writer = ThreadPoolExecutor(max_workers=1)
 
     def start(self):
-        if self._shutting_down:
-            return
         self._reader.submit(self._stdin_read)
 
     def stop(self):
-        self._shutting_down = True
         self._reader.shutdown()
-        self._writer.shutdown()
         sys.stdout.close()
 
     def write(self, data):
-        if self._shutting_down:
-            return
-        self._writer.submit(self._stdout_write, data)
-
-    def _stdout_write(self, data):
-        # Do not call directly - only invoked by the _writer executor
-        try:
-            sys.stdout.write(data)
-            sys.stdout.write("\n")
-            sys.stdout.flush()
-        except:
-            logging.exception("Exception when writing data to stdout:")
-            raise
+        sys.stdout.write(data)
+        sys.stdout.write("\n")
+        sys.stdout.flush()
 
     def _stdin_read(self):
         # Do not call directly - only invoked by the _reader executor
