be705cc07eecbd9c198ba7df9b39a3119f3d832e
Geoffrey Yu <geoffxy@gmail.com>
2018-01-05 14:57:36 -0500

    Move message handling into separate functions

--- agent/tandem/protocol/editor/handler.py
+++ agent/tandem/protocol/editor/handler.py
@@ -12,32 +12,34 @@ class EditorProtocolHandler:
         try:
             message = m.deserialize(data)
             if type(message) is m.ConnectTo:
-                # Connect to another agent
-                logging.info(
-                    "Tandem Agent is attempting to establish a "
-                    "connection to {}:{}.".format(message.host, message.port),
-                )
-                self._connection_manager.connect_to(message.host, message.port)
-                logging.info(
-                    "Tandem Agent connected to {}:{}."
-                    .format(message.host, message.port),
-                )
-
-                # Ping the agent
-                address = (message.host, message.port)
-                connection = self._connection_manager.get_connection(address)
-                if connection is None:
-                    logging.error("Could not find connection!")
-                    return
-                ping = im.Ping(5)
-                connection.write_string_message(im.serialize(ping))
+                self._handle_connect_to(message)
             else:
                 # Echo the message back - placeholder behaviour
                 response = m.serialize(message)
                 self._std_streams.write_string_message(response)
         except m.EditorProtocolMarshalError:
-            logging.info("Ignoring invalid message.")
+            logging.info("Ignoring invalid editor protocol message.")
         except:
             logging.exception(
                 "Exception when handling editor protocol message:")
             raise
+
+    def _handle_connect_to(self, message):
+        logging.info(
+            "Tandem Agent is attempting to establish a "
+            "connection to {}:{}.".format(message.host, message.port),
+        )
+        self._connection_manager.connect_to(message.host, message.port)
+        logging.info(
+            "Tandem Agent connected to {}:{}."
+            .format(message.host, message.port),
+        )
+
+        # Ping the agent (this is for testing)
+        address = (message.host, message.port)
+        connection = self._connection_manager.get_connection(address)
+        if connection is None:
+            logging.error("Could not find connection!")
+            return
+        ping = im.Ping(2)
+        connection.write_string_message(im.serialize(ping))
--- agent/tandem/protocol/interagent/handler.py
+++ agent/tandem/protocol/interagent/handler.py
@@ -6,20 +6,23 @@ class InteragentProtocolHandler:
     def __init__(self, connection_manager):
         self._connection_manager = connection_manager
 
-    def handle_message(self, data, address):
+    def handle_message(self, data, sender_address):
         try:
             message = m.deserialize(data)
             if type(message) is m.Ping:
-                logging.info("Received ping with TTL {}.".format(message.ttl))
-                if message.ttl <= 0:
-                    return
-                # Ping the sender back
-                sender_connection = \
-                    self._connection_manager.get_connection(address)
-                sender_connection.write_string_message(m.serialize(m.Ping(message.ttl-1)))
-        except m.EditorProtocolMarshalError:
-            pass
+                self._handle_ping(message, sender_address)
+        except m.InteragentProtocolMarshalError:
+            logging.info("Ignoring invalid interagent protocol message.")
         except:
             logging.exception(
                 "Exception when handling interagent protocol message:")
             raise
+
+    def _handle_ping(self, message, sender_address):
+        logging.info("Received ping with TTL {}.".format(message.ttl))
+        if message.ttl <= 0:
+            return
+        # Ping the sender back
+        sender_connection = \
+            self._connection_manager.get_connection(sender_address)
+        sender_connection.write_string_message(m.serialize(m.Ping(message.ttl-1)))
