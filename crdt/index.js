017a9a246101e09c52ac20aea70ce3f2d9318baa
Jamiboy Mohammad <jamiboy16@gmail.com>
2018-01-12 08:28:51 -0500

    Added IO to crdt node process

--- crdt/index.js
+++ crdt/index.js
@@ -2,15 +2,16 @@ import uuid from 'uuid';
 import { Document } from '@atom/teletype-crdt';
 import DocumentStore from './stores/document';
 import api from './api';
-// import io from './io';
+import io from './io';
 
 const localDocument = new Document({ siteId: uuid(), text: '' });
 DocumentStore.setDocument(localDocument);
 
-console.log(api.setTextInRange(
-    { column: 0, row: 0 },
-    { column: 0, row: 0 },
-    'hello, world!',
-));
-console.log();
-console.log(api.getDocumentText());
+const processPayload = (payload) => {
+    const { function: fcn, parameters: params = [] } = JSON.parse(payload);
+    const res = { value: api[fcn](...params) };
+
+    io.write(JSON.stringify(res));
+};
+
+io.addIoHandler(processPayload);
--- crdt/io/index.js
+++ crdt/io/index.js
@@ -1,1 +1,21 @@
-export default 'io';
+const ioHandlers = [];
+let inPayload = '';
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+process.stdin.on('data', (data) => { inPayload += data; });
+process.stdin.on('end', () => {
+    ioHandlers.forEach(handler => handler(inPayload));
+    inPayload = '';
+});
+
+const addIoHandler = handler => ioHandlers.push(handler);
+const write = (outPayload) => {
+    process.stdout.write(outPayload);
+    process.stdout.write('\n');
+};
+
+export default {
+    addIoHandler,
+    write,
+};
