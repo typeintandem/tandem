3aa19821ad4a89b3d868106320e0b11a36ad373b
Sameer Chitley <chitley.sameer@gmail.com>
2018-02-16 00:02:42 -0500

    Add apm link/unlin to dev setup

--- plugins/atom/atom_dev_setup.sh
+++ plugins/atom/atom_dev_setup.sh
@@ -5,15 +5,20 @@ SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 UNINSTALL_OPTION="--uninstall"
 
 function install() {
-  uninstall
+  rm -f agent
+  rm -f crdt
 
   ln -s $SCRIPTPATH/../../agent $SCRIPTPATH
   ln -s $SCRIPTPATH/../../crdt $SCRIPTPATH
+
+  apm link .
 }
 
 function uninstall() {
   rm -f agent
   rm -f crdt
+
+  apm unlink .
 }
 
 if [ "$1" == "$UNINSTALL_OPTION" ] ; then
