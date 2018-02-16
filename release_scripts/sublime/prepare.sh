d7e63e2b192a046333cc699759f84f25aad0d157
Sameer Chitley <chitley.sameer@gmail.com>
2018-02-16 12:44:01 -0500

    Build crdt with dependencies before copying

--- release_scripts/sublime/prepare.sh
+++ release_scripts/sublime/prepare.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Given a destination, this script builds the sublime
-# plugin for distribution at the destination. The
-# destination supplised should be a local copy of the
+# Given a destination, this script builds the sublime plugin for distribution
+# at the destination. The destination supplised should be a local copy of the
 # plugin repository.
-
-# NOTE: It is the script invoker's responsibility to make sure all dependencies
-# for the crdt have been built, etc.
 
 if [[ "$1" == "" ]]; then
   echo "Please supply a path to the plugin target destination."
@@ -24,13 +20,24 @@ rm -f $INSTALL_PATH/*.py
 rm -f $INSTALL_PATH/*.sublime-*
 rm -f $INSTALL_PATH/README.md
 
+# Create agent and crdt subdirectories
 mkdir $INSTALL_PATH/agent/
 mkdir $INSTALL_PATH/crdt/
 
+# Agent
 cp -r $SCRIPT_PATH/../../agent/ $INSTALL_PATH/agent/
 
+# CRDT
+$(
+  cd $SCRIPT_PATH/../../crdt/;
+  npm run clean;
+  rm -rf node_modules;
+  npm install;
+  npm run build
+)
 cp -r $SCRIPT_PATH/../../crdt/ $INSTALL_PATH/crdt/
 
+# Sublime specific files
 cd $SCRIPT_PATH/../../plugins/sublime/
 cp -r enum-dist/ $INSTALL_PATH/enum-dist/
 cp *.py $INSTALL_PATH
