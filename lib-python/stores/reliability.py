0ccb07e52f6e8d3885125b15580073e220169b8a
Jamiboy Mohammad <jamiboy16@gmail.com>
2018-03-09 17:52:21 -0500

    First pass on reliability

---
+++ lib-python/stores/reliability.py
@@ -0,0 +1,15 @@
+from tandem.shared.stores.base import StoreBase
+
+
+class ReliabilityStore(StoreBase):
+    def __init__(self):
+        self._payloads = {}
+
+    def add_payload(self, payload_id, payload):
+        self._payloads[payload_id] = payload
+
+    def get_payload(self, payload_id):
+        return self._payloads.get(payload_id, None)
+
+    def remove_payload(self, payload_id):
+        del self._payloads[payload_id]
---
+++ lib-python/utils/reliability.py
@@ -0,0 +1,66 @@
+from tandem.shared.stores.reliability import ReliabilityStore
+
+
+class ReliabilityUtils(object):
+    HEADER = b"\x54\x01"
+    RELIABILITY_HEADER = b"\x41\x43"
+    ACK_HEADER = b"\x41\x43"
+    ACK_TIMEOUT = 3
+
+    MAX_ACK_NUMBER = int(0xFFFF)
+    next_ack_number = -1
+
+    @classmethod
+    def get_next_ack_number(cls):
+        cls.next_ack_number += 1
+        cls.next_ack_number %= cls.MAX_SEQUENCE_NUMBER + 1
+
+        return cls.next_ack_number
+
+    @classmethod
+    def is_ack(cls, raw_data):
+        return (
+            raw_data[0:2] == cls.HEADER and
+            raw_data[2:4] == cls.ACK_HEADER
+        )
+
+    @classmethod
+    def is_ackable(cls, raw_data):
+        return (
+            raw_data[0:2] == cls.HEADER and
+            raw_data[2:4] == cls.RELIABILITY_HEADER
+        )
+
+    @staticmethod
+    def should_resend_payload(ack_id):
+        return ReliabilityStore.get_instance().get_payload(ack_id)
+
+    @staticmethod
+    def generate_ack(ack_id):
+        result = []
+        result.append(ReliabilityUtils.HEADER)
+        result.append(ReliabilityUtils.ACK_HEADER)
+        result.append((ack_id).to_bytes(2, byteorder="big"))
+        return b"".join(result)
+
+    @staticmethod
+    def parse_ack(raw_data):
+        return int.from_bytes(raw_data[2:4], byteorder="big")
+
+    @staticmethod
+    def serialize(payload):
+        result = []
+        result.append(ReliabilityUtils.HEADER)
+        result.append(ReliabilityUtils.RELIABILITY_HEADER)
+        result.append(ReliabilityUtils.get_next_ack_number().to_bytes(
+            2,
+            byteorder="big",
+        ))
+        result.append(payload)
+        return b"".join(result)
+
+    @staticmethod
+    def deserialize(raw_data):
+        ack_id = raw_data[2:4]
+        payload = raw_data[4:]
+        return payload, ack_id
